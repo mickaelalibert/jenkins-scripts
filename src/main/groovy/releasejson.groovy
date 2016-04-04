7a5febe80a187506a1edae542bc3f8a219533ffc
Nicolas Géraud <nicolas.geraud@gmail.com>
2016-04-04 23:41:19 +0200

    fix(release): git commands must be run under ROOT home

--- src/main/groovy/releasejson.groovy
+++ src/main/groovy/releasejson.groovy
@@ -1,25 +1,27 @@
 def updateReleaseJson(json, tag, dryRun) {
     node {
         ws {
-            sh "rm -rf *"
-            sh "rm -rf .git"
-            git url: 'git@github.com:gravitee-io/release.git', branch: "master"
+            withEnv(["HOME=/root"]) {
+                sh "rm -rf *"
+                sh "rm -rf .git"
+                git url: 'git@github.com:gravitee-io/release.git', branch: "master"
 
-            stage "Update release.json"
-            println(json)
-            writeFile file: 'release.json', text: json
+                stage "Update release.json"
+                println(json)
+                writeFile file: 'release.json', text: json
 
-            sh "git add --update"
-            if (tag != null) {
-                sh "git commit -m 'release(${tag})'"
-                stage "Tag release.json ${tag}"
-                sh "git tag ${tag}"
-            } else {
-                sh "git commit -m 'prepareRelease(): update version'"
-            }
+                sh "git add --update"
+                if (tag != null) {
+                    sh "git commit -m 'release(${tag})'"
+                    stage "Tag release.json ${tag}"
+                    sh "git tag ${tag}"
+                } else {
+                    sh "git commit -m 'prepareRelease(): update version'"
+                }
 
-            if (!dryRun) {
-                sh "git push --tags origin master"
+                if (!dryRun) {
+                    sh "git push --tags origin master"
+                }
             }
         }
 
