53daf29d39a786147789a52d8897c933eeb51754
Nicolas Géraud <nicolas.geraud@gmail.com>
2016-11-19 12:29:57 +0100

    fix(docker): add --rm to docker run commands

--- src/main/groovy/generateChangelog.groovy
+++ src/main/groovy/generateChangelog.groovy
@@ -9,7 +9,7 @@ node {
             submoduleCfg: [],
             userRemoteConfigs: [[credentialsId: '6a9f959a-dc73-4c4a-a998-049d3c725d34', url: 'git@github.com:gravitee-io/issues.git']]])
 
-    sh "docker run --env MILESTONE_VERSION=${MILESTONE_VERSION} -v '$WORKSPACE':/data graviteeio/changelog"
+    sh "docker run --rm --env MILESTONE_VERSION=${MILESTONE_VERSION} -v '$WORKSPACE':/data graviteeio/changelog"
 
     echo readFile("CHANGELOG.md")
 
