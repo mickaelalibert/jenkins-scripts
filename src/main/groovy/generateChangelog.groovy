2e030da39f2324b8661a0d23859132c9589963c2
Nicolas Géraud <nicolas.geraud@gmail.com>
2019-02-18 11:36:41 +0100

    changelog: upgrade script to manage 'APIM - x.y.z'

--- src/main/groovy/generateChangelog.groovy
+++ src/main/groovy/generateChangelog.groovy
@@ -9,13 +9,14 @@ node {
             submoduleCfg: [],
             userRemoteConfigs: [[credentialsId: '6a9f959a-dc73-4c4a-a998-049d3c725d34', url: 'git@github.com:gravitee-io/issues.git']]])
 
-    sh "docker run --rm --env MILESTONE_VERSION=${MILESTONE_VERSION} -v '$WORKSPACE':/data graviteeio/changelog"
+    sh "docker run --rm --env MILESTONE_VERSION='${MILESTONE_VERSION}' -v '$WORKSPACE':/data graviteeio/changelog"
 
     echo readFile("CHANGELOG.adoc")
 
     sh "git add --update"
-    sh "git commit -m \"Generate changelog for version ${MILESTONE_VERSION}\""
-    sh "git tag ${MILESTONE_VERSION}"
+    replacement = "${MILESTONE_VERSION}".replace(" ", "_")
+    sh "git commit -m \"Generate changelog for version ${replacement}\""
+    sh "git tag \"${replacement}\""
 
     if (!Boolean.valueOf(dryRun)) {
         sh "git push --tags origin master"
